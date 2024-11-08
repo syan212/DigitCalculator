f47da269cb7bfa2be968b045ffcb0b26cecc3ca9
syan212 <syan235711@gmail.com>
2024-11-08 08:21:13 +0000

    hopefully it works

--- script.js
+++ script.js
@@ -20,6 +20,15 @@ function back(){
             }
 //Complex mode
 function complex(){
+                  var checkBox = document.getElementById("complexcheck");
+                   // Get the output text
+                  var text = document.getElementById("complex");
 
+                  // If the checkbox is checked, display the output text
+                  if (checkBox.checked == true){
+                    text.style.display = "block";
+                  } else {
+                    text.style.display = "none";
+                  }
 }
 
