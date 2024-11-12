12332a06ea02e759955b5fd50637161c329b3776
syan212 <syan235711@gmail.com>
2024-11-12 21:35:10 +0000

    maybe

--- script.js
+++ script.js
@@ -95,7 +95,7 @@ function opentab(evt, tab) {
   }
 
   // Get all elements with class="tablinks" and remove the class "active"
-  tablinks = document.getElementsByClassName("tab");
+  tablinks = document.getElementsByClassName(tab);
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
