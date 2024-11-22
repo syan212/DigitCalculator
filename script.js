4944447a132315d58db1490e29413d84f5e25d86
syan212 <syan235711@gmail.com>
2024-11-22 20:44:20 +0000

    ur mum

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function opentab(evt, tab) {
   }
   
   tabs = document.getElementsByClassName("tabs");
-  for (i = 0; i < tablinks.length; i++) {
+  for (i = 0; i < tabs.length; i++) {
     tabs[i].className = tabs[i].className.replace(" active", "");
   }
 
